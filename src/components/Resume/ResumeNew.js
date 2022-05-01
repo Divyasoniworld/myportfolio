8c87b9e83ec4e60791a19ecf956056651ce777eb
divyasoniworld <divyasoniworld@gmail.com>
2022-05-01 11:49:36 +0530

    initial commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/divadarshansoni.pdf";
+import pdf from "../../Assets/../Assets/divyadarshansoni.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/divyasoniworld/myportfolio/main/src/Assets/divyadarshansoni.pdf";
+  "https://raw.githubusercontent.com/divyasoniworld/myportfolio/blob/main/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
